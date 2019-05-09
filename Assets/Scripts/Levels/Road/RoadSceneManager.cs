badb7ebc0a1abefe99aa8971c522347f51b2dd88
LASER-Yi <ly0007@yeah.net>
2019-05-09 16:01:18 +0800

    更新页面布局

---
+++ Assets/Scripts/Levels/Road/RoadSceneManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LGameDataStruct;
+
+public class RoadSceneManager : SceneBaseController
+{
+    private CarController m_PlayerCar;
+    [SerializeField]
+    private GameObject m_EndPoint;
+
+    private RoadSceneParam m_SceneParam;
+    new void Start()
+    {
+        base.Start();
+        m_PlayerCar = GameObject.FindGameObjectWithTag("Player").GetComponent<CarController>();
+        m_PlayerCar.setEnginePower(0.4f);
+
+        GameManager.instance.setTimeDelta(0.5f);
+        GameManager.instance.setTimeSpeed(18f);
+        GameManager.instance.startTimeLoop();
+    }
+
+    public float getPlayerVelocity()
+    {
+        return m_PlayerCar.getVelocity();
+    }
+
+    public float getDistanceToEndPoint()
+    {
+        return Vector3.Distance(m_PlayerCar.transform.position, m_EndPoint.transform.position);
+    }
+
+    void Update()
+    {
+        if (getDistanceToEndPoint() < 20f)
+        {
+            handleCarReachDestination();
+        }
+    }
+
+    void handleCarReachDestination()
+    {
+        loadNextScene();
+    }
+
+    void loadNextScene()
+    {
+        GSceneController.instance.LoadNextScene(true);
+    }
+}
---
+++ Assets/Scripts/UI/UiEscapePanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiEscapePanel : MonoBehaviour, IStackableUi
+{
+    [SerializeField]
+    private Button m_ResumeButton;
+    [SerializeField]
+    private Button m_ExitButton;
+
+    void Start(){
+        m_ResumeButton.onClick.AddListener(clickResumeButton);
+        m_ExitButton.onClick.AddListener(clickExitButton);
+    }
+
+    public void clickResumeButton(){
+        GCanvasController.instance.popStack();
+    }
+
+    public void clickExitButton(){
+        SceneBaseController.instance.backToMenu();
+    }
+    public RectTransform getTransform(){
+        return transform as RectTransform;
+    }
+    public void onDidPushToStack(bool animate){
+
+    }
+    public void onDidBecomeTop(){
+
+    }
+    public void onWillNotBecomeTop(){
+
+    }
+    public float onWillRemoveFromStack(bool animate){
+        return 0f;
+    }
+}
