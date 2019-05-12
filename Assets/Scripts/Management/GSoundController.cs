ca87b11e11f8367a38ed5327fb85a76eb396bb2c
LASER-Yi <ly0007@yeah.net>
2019-05-12 10:26:18 +0800

    增加全局音频控制器

---
+++ Assets/Scripts/Management/GSoundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GSoundController : MonoBehaviour
+{
+    private static Object _lock = new Object();
+    private static GSoundController _instance;
+
+    public static GSoundController instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    var obj = FindObjectOfType<GSoundController>();
+                    if (obj != null)
+                    {
+                        _instance = obj;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+    [SerializeField]
+    private AudioSource m_EffectSource;
+
+    [SerializeField]
+    private AudioClip m_ButtonPressClip;
+}
